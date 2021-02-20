231c538e424c3757d7c87c8d5718df2b938209cb
Maxflo28 <Maxflo13@gmail.com>
2021-02-20 14:20:50 -0800

    adding tip project

---
+++ tip_calculator.py
@@ -0,0 +1,33 @@
+# Make a python script tip_calculator.py that takes a user's input at the command line for:
+
+#     Cost of the food
+#     Number of people splitting the bill
+#     Percentage of the tip
+
+# Then, the script should output:
+
+#     The total bill (including tip)
+#     how much each person should pay
+
+# Assume there is a 10% sales tax. Don't forget to add this into the total bill!
+# Food price variable will be a float(whole number)
+Food_price = float(input("Enter your meal amount: "))
+# tip amount will be a int(decimal)
+Tip = int(input("Enter tip %: "))
+# split will input how much to divide total cost 
+Split = float(input("Enter number of guests "))
+Tax = .10
+
+# calculations
+# tip amount variable will be equal to food price multiplied by tip/100. 100 will represrent whole dollar
+Tip_amt = Food_price * Tip/100
+# tax amount is food multiplied by tax 
+Tax_amt = Food_price * Tax
+Total = Food_price + Tip_amt + Tax_amt 
+Total_amt_owed = Total/Split
+
+# F string outputs will give us the results based on calculations 
+# Final output will show how much is owed per guest tax included
+print(f"Your meal was {Food_price} and your tip was {Tip_amt}.")
+print(f"Your total with tax is {Total} ")
+print(f"Split between {Split} guests, you owe {Total_amt_owed}")
